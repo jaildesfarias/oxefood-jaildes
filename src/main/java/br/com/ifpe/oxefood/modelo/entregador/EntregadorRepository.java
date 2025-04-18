d6f54f16c3b3274afca2021f4e46f738522ee68a
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2025-04-17 22:53:05 -0300

    Update EntregadorRepository.java
     Você pode adicionar métodos customizados aqui se precisar (ex: buscar por nome, CPF etc.)
     Implemente no projeto do back-end o cadastro de:

--- src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorRepository.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorRepository.java
@@ -1,5 +1,10 @@
 package br.com.ifpe.oxefood.modelo.entregador;
 
-public interface EntregadorRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // Indica que essa interface é um repositório do Spring
+public interface EntregadorRepository extends JpaRepository<Entregador, Long> {
+    // Você pode adicionar métodos customizados aqui se precisar (ex: buscar por nome, CPF etc.)
 }
+
