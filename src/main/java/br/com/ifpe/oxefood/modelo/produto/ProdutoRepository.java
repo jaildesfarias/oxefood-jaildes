e1f395440863bbbc6618c1abe18c67c45a1c9605
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2025-04-17 23:29:52 -0300

    Update ProdutoRepository.java
    1. Exercício
     Implemente no projeto do back-end o cadastro de:
    ProdutoResitory.java

--- src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoRepository.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoRepository.java
@@ -1,5 +1,11 @@
 package br.com.ifpe.oxefood.modelo.produto;
 
-public class ProdutoRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // Marca a interface como um componente de acesso a dados
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    //Pode criar métodos personalizados aqui se quiser, por exemplo:
+    // List<Produto> findByTituloContaining(String termo);
 }
+
