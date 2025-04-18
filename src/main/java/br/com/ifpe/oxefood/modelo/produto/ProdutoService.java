8211a9738f566f1fc3ccc9a1aa69162f8c7155db
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2025-04-17 23:38:27 -0300

    Update ProdutoService.java
              Exercício
    1) Implemente no projeto do back-end o cadastro de:
    ProdutoService.java

--- src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
@@ -1,5 +1,22 @@
 package br.com.ifpe.oxefood.modelo.produto;
 
-public interface ProdutoService {
-    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service // Indica que essa classe é um serviço gerenciado pelo Spring
+public class ProdutoService {
+
+    @Autowired // Injeta automaticamente o repositório
+    private ProdutoRepository produtoRepository;
+
+    public Produto save(Produto produto) {// Salva um novo produto no banco
+        return produtoRepository.save(produto);
+    }
+
+    // Pode adicionar outros métodos úteis como:
+    // - buscar por ID
+    // - listar todos
+    // - atualizar produto
+    // - deletar produto
 }
+
