a00e820e2b154c5e3a246cc4d88bb9bfa6997cef
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2025-04-29 23:22:59 -0300

    Update ProdutoService.java
    LISTAGEM

--- src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
@@ -1,5 +1,5 @@
 package br.com.ifpe.oxefood.modelo.produto;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +12,18 @@ public class ProdutoService {
     public Produto save(Produto produto) {// Salva um novo produto no banco
         return produtoRepository.save(produto);
     }
+
+    //listagem
+    public List<Produto> listarTodos() {
+  
+        return repository.findAll();//findAll();SELECT *FROM(CLIENTE
+    }
+
+    public produto obterPorID(Long id) {
+
+        return repository.findById(id).get();//VAI FILTRA TODO ID SELECT *FROM(CLIENTE WHERE ID = ? PASSA POR PARAMENTRO
+    }
+
 
     // Pode adicionar outros métodos úteis como:
     // - buscar por ID
