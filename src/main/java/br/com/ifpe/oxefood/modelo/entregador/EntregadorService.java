0bff30df17f0e0ba2df5e0a9f3f5f815aa30dd6a
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2025-05-04 21:15:08 -0300

    Update EntregadorService.java

--- src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
@@ -4,8 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import br.com.ifpe.oxefood.modelo.cliente.Cliente;
+import org.springframework.transaction.annotation.Transactional; // Import necessário
 
 @Service
 public class EntregadorService {
@@ -24,40 +23,38 @@ public class EntregadorService {
     }
 
     public Entregador obterPorID(Long id) {
-        return entregadorRepository.findById(id).orElse(null); // ou lançar uma exceção customizada
+        return entregadorRepository.findById(id).orElse(null);
     }
 
     @Transactional
     public void update(Long id, Entregador entregadorAlterado) {
- 
-       Entregador entregador = repository.findById(id).get();
-       entregador.setNome(entregadorAlterado.getNome());
-       entregador.setCpf(entregadorAlterado.getCpf());
-       entregador.setDataNascimento(entregadorAlterado.getDataNascimento());
-       entregador.setFoneCelular(entregadorAlterado.getFoneCelular());
-       entregador.setFoneFixo(entregadorAlterado.getFoneFixo());
-  
-  
-    
 
-      entregador.setQtdEntregasRealizadas( entregadorAlterado.getQtdEntregasRealizadas());
-      entregador.setValorFrete( entregadorAlterado.getValorFrete());
-      entregador.setRua( entregadorAlterado.getRua());
-      entregador.setComplemento(entregadorAlterado.getComplemento());
-      entregador.setNumero(entregadorAlterado.getNumero());
+        Entregador entregador = entregadorRepository.findById(id).get();
 
-      entregador.setBairro( entregadorAlterado.getBairro());
-      entregador.setCidade( entregadorAlterado.getCidade());
-      entregador.setCep( entregadorAlterado.getCep());
-      entregador.setUf(entregadorAlterado.getUf());
-      entregador.setAtivo(entregadorAlterado.getAtivo());
-	    
-      repository.save(entregador);
-  }
+        entregador.setNome(entregadorAlterado.getNome());
+        entregador.setCpf(entregadorAlterado.getCpf());
+        entregador.setDataNascimento(entregadorAlterado.getDataNascimento());
+        entregador.setFoneCelular(entregadorAlterado.getFoneCelular());
+        entregador.setFoneFixo(entregadorAlterado.getFoneFixo());
 
+        entregador.setQtdEntregasRealizadas(entregadorAlterado.getQtdEntregasRealizadas());
+        entregador.setValorFrete(entregadorAlterado.getValorFrete());
+        entregador.setRua(entregadorAlterado.getRua());
+        entregador.setComplemento(entregadorAlterado.getComplemento());
+        entregador.setNumero(entregadorAlterado.getNumero());
+        entregador.setBairro(entregadorAlterado.getBairro());
+        entregador.setCidade(entregadorAlterado.getCidade());
+        entregador.setCep(entregadorAlterado.getCep());
+        entregador.setUf(entregadorAlterado.getUf());
+        entregador.setAtivo(entregadorAlterado.getAtivo());
 
-    // Possíveis métodos adicionais:
-    // - atualizar
-    // - deletar
-    // - buscar por filtros, etc.
+        entregadorRepository.save(entregador);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Entregador entregador = entregadorRepository.findById(id).get();
+        entregador.setAtivo(Boolean.FALSE);
+        entregadorRepository.save(entregador);
+    }
 }
