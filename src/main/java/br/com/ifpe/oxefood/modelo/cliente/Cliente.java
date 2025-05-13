de3b82f58548c4c3e7280ce2fc4a881afc4ec997
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2025-05-13 10:54:54 -0300

    Update Cliente.java
    alteração

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/Cliente.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/Cliente.java
@@ -8,6 +8,9 @@ import br.com.ifpe.oxefood.util.entity.EntidadeAuditavel;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,23 +24,24 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Cliente extends EntidadeAuditavel  {
+public class Cliente extends EntidadeAuditavel {
 
-   @Column
-   private String nome;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // ID é necessário para a JPA funcionar corretamente
+
+    @Column(nullable = false)
+    private String nome;
 
     @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataNascimento;
 
-   private LocalDate dataNascimento;
+    @Column(nullable = false, unique = true)
+    private String cpf;
 
-   @Column
-   private String cpf;
+    @Column
+    private String foneCelular;
 
-
-   @Column
-   private String foneCelular;
-   
-   @Column
-   private String foneFixo;
-
+    @Column
+    private String foneFixo;
 }
