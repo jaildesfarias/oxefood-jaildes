45214d0f828c5091ff0b59de0ef3ca6a5dcd6be2
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2025-05-13 10:56:18 -0300

    Update ClienteRepository.java

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteRepository.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteRepository.java
@@ -1,9 +1,16 @@
 package br.com.ifpe.oxefood.modelo.cliente;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
-  
+  List<Cliente> findByNome(String nome);// Busca todos os entregadores com o nome exato informado
+
+    Optional<Cliente> findByCpf(String cpf); 
 
 }
