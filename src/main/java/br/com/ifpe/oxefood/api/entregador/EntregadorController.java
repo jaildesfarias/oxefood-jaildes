f8e1311c67f71e1dda74781e38edc2e3a54aa284
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2025-05-13 10:44:46 -0300

    Update EntregadorController.java
    Implementando a Alteração

--- src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
+++ src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
@@ -1,45 +1,18 @@
 package br.com.ifpe.oxefood.api.entregador;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-=======
-import org.springframework.web.bind.annotation.GetMapping;      
-import org.springframework.web.bind.annotation.PathVariable; 
->>>>>>> 1c3cc6ed70d483072e68ba87dd7c97c5875b2013
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> 1c3cc6ed70d483072e68ba87dd7c97c5875b2013
->>>>>>> 65b46cd273066d6d92163bd2abd55a0db8046642
 import br.com.ifpe.oxefood.modelo.entregador.Entregador;
 import br.com.ifpe.oxefood.modelo.entregador.EntregadorService;
-
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 1c3cc6ed70d483072e68ba87dd7c97c5875b2013
-@RestController
-@RequestMapping("/api/entregador")
 @CrossOrigin
+
+
+@RequestMapping("/api/entregador")
+@RestController
 public class EntregadorController {
 
     @Autowired
@@ -48,48 +21,34 @@ public class EntregadorController {
     @PostMapping
     public ResponseEntity<Entregador> save(@RequestBody EntregadorRequest request) {
         Entregador entregador = entregadorService.save(request.build());
+        Entregador save = entregadorService.save(entregador);
         return new ResponseEntity<>(entregador, HttpStatus.CREATED);
     }
 
-     @GetMapping
+    @GetMapping
     public List<Entregador> listarTodos() {
         return entregadorService.listarTodos();
     }
 
     @GetMapping("/{id}")
-    public Entregador obterPorID(@PathVariable Long id) {
-        return entregadorService.obterPorID(id);
+    public ResponseEntity<Entregador> obterPorID(@PathVariable Long id) {
+        Entregador entregador = entregadorService.obterPorID(id);
+        if (entregador != null) {
+            return ResponseEntity.ok(entregador);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
-<<<<<<< HEAD
 
-    
- @PutMapping("/{id}")
-=======
-    @PutMapping("/{id}")//id do cliente
->>>>>>> 65b46cd273066d6d92163bd2abd55a0db8046642
- public ResponseEntity<Entregador> update(@PathVariable("id") Long id, @RequestBody EntregadorRequest request) {
+    @PutMapping("/{id}")
+    public ResponseEntity<Entregador> update(@PathVariable("id") Long id, @RequestBody EntregadorRequest request) {
+        entregadorService.update(id, request.build());
+        return ResponseEntity.ok().build();
+    }
 
-       entregadorService.update(id, request.build());//
-       return ResponseEntity.ok().build();
- }
-<<<<<<< HEAD
-  @DeleteMapping("/{id}")
-   public ResponseEntity<Void> delete(@PathVariable Long id) {
-
-       entregadorService.delete(id);
-       return ResponseEntity.ok().build();
-   }
-
-
-=======
- @DeleteMapping("/{id}")
-public ResponseEntity<Void> delete(@PathVariable Long id) {
-    entregadorService.delete(id);
-    return ResponseEntity.ok().build();
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        entregadorService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
-
- 
->>>>>>> 1c3cc6ed70d483072e68ba87dd7c97c5875b2013
-
-}
-
